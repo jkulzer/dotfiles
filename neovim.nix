297f14cb1de83e11fa5e225db3455d1305bb120e
jkulzer <git@jkulzer.dev>
2023-09-13 21:59:07 +0200

    added python lsp and todo-comments plugin

--- neovim.nix
+++ neovim.nix
@@ -1,18 +1,25 @@
 {
   nixvim = {
     enable = true;
+    clipboard.providers.wl-copy.enable = true; # Enable the wayland clipboard
     colorschemes.catppuccin = {
       enable = true;
       flavour = "mocha";
     };
+    maps = {
+      normal."<space>ee" = {
+	silent = true;
+	action = "<cmd>lua vim.diagnostic.open_float()<CR>"; # Expands the LSP error message (useful when the message is longer than a line, because the LSP doesn't have line wrap
+      };
+    };
     plugins = {
-      # This gives every parenthesis a different color
-      rainbow-delimiters.enable = true;
-      treesitter.enable = true;
+      rainbow-delimiters.enable = true; # This gives every parenthesis a different color
+      treesitter.enable = true; # Fancy Syntax highlighting plugin
       cmp-dictionary.enable = true;
       cmp-nvim-lsp.enable = true;
       comment-nvim.enable = true; # Provides easy keybindings to comment a line or multiple lines
       airline.enable = true; # Fancy statusbar
+      todo-comments.enable = true; # Shows the TODO comments in a colorful way
       coq-nvim = {
 	enable = true; # Autocompletion display
 	autoStart = true;
@@ -25,6 +32,7 @@
 	  nil_ls.enable = true; # Nix LSP
 	  jsonls.enable = true; # JSON LSP
 	  yamlls.enable = true; # YAML LSP
+	  pylsp.enable = true; # Python LSP
 	};
       };
       telescope = {
